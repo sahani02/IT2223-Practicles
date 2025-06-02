d1b13f64dd02aef936f9caca3ea176ea86f37a3c
sahani02 <2002sahani@gmail.com>
2025-06-02 21:55:49 +0530

    Add files via upload

---
+++ DAY 1/first.m
@@ -0,0 +1,21 @@
+num=4;
+if num>0
+    disp('the number is positive')
+else
+    disp('the num is negative')
+end
+
+if mod(num,2)==0
+    disp('the num is even')
+else
+    disp('the num is odd')
+end
+
+num=1;
+while num<=5
+    disp(num)
+    num=num+1;
+end
+
+A=["add" "kpp"; "noo" "ihh"];
+disp(A)
