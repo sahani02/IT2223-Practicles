74b4ca15a025327bd2d1275704970db2295087e8
sahani02 <2002sahani@gmail.com>
2025-06-10 22:01:13 +0530

    Delete PRACTICALS/Graphs/WUndirected.m

--- PRACTICALS/Graphs/WUndirected.m
+++
@@ -1,6 +0,0 @@
-s=[1 2 2];% start node
-t=[2 3 4];% end node
-weights=[10 5 7];
-G=graph(s, t, weights);% create graph object
-plot(G, 'EdgeLabel',G.Edges.Weight);
-title('Waited undirected Graph');
